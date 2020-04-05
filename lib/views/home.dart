5a6493198259680074f468ed3809460e9aea1482
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-05 11:18:11 +0700

    add: active donation list

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:rmi/style.dart';
 
 class Home extends StatefulWidget {
@@ -10,17 +12,23 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mainBgColor,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             _buildTopStack(),
             SizedBox(
-              height: 100.0,
+              height: 50.0,
             ),
-            Container(
-              height: 100.0,
-              color: Colors.red,
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                children: <Widget>[
+                  _buildDonationHeader(),
+                  _buildDonationList(),
+                ],
+              ),
             ),
           ],
         ),
@@ -34,7 +42,7 @@ class _HomeState extends State<Home> {
       overflow: Overflow.visible,
       children: <Widget>[
         Container(
-          color: Colors.green,
+          color: mainForeColor,
           height: 210.0,
         ),
         Center(
@@ -64,8 +72,8 @@ class _HomeState extends State<Home> {
         Positioned(
           bottom: -40,
           child: Container(
-            height: 85,
-            width: MediaQuery.of(context).size.width - 55,
+            height: 80,
+            width: MediaQuery.of(context).size.width - 50,
             padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
               color: Colors.white,
@@ -78,6 +86,46 @@ class _HomeState extends State<Home> {
           ),
         ),
       ],
+    );
+  }
+
+  _buildDonationHeader() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "Bantu siapa hari ini?",
+            style: headerTextStyle,
+          ),
+          Text(
+            "Lihat Semua",
+            style: headerTextLinkStyle,
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildDonationList() {
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return Container(
+      child: ListView.builder(
+          itemCount: entries.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(top: 5.0, bottom: 5.0),
+              padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 18.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(child: Text('Entry ${entries[index]}')),
+            );
+          }),
     );
   }
 }
