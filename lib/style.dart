60a6bf1707d232b7a9372f5202acd33769489bf3
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-05 10:22:59 +0700

    init: style guide

---
+++ lib/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const donationText = TextStyle(
+  color: Colors.white,
+  fontSize: 30.0,
+  fontWeight: FontWeight.w600,
+);
+
+const donationSubText = TextStyle(
+  color: Colors.white,
+  fontSize: 15.0,
+);
+
+const donationSubTextItalic = TextStyle(
+  color: Colors.white60,
+  fontSize: 13.0,
+  fontStyle: FontStyle.italic,
+);
