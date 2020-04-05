e192d15b581565152a901f1b1a875201bf7f3cb6
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-05 10:23:29 +0700

    create card info

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rmi/style.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,13 +10,74 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Center(child: Text("Ruang Muda Indramayu")),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _buildTopStack(),
+            SizedBox(
+              height: 100.0,
+            ),
+            Container(
+              height: 100.0,
+              color: Colors.red,
+            ),
+          ],
+        ),
       ),
-      body: Container(
+    );
+  }
 
-      ),
+  Stack _buildTopStack() {
+    return Stack(
+      alignment: AlignmentDirectional.topCenter,
+      overflow: Overflow.visible,
+      children: <Widget>[
+        Container(
+          color: Colors.green,
+          height: 210.0,
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Donasi terkumpul",
+                  style: donationSubText,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text("Rp. 15.000.000", style: donationText),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "* Sampai 01 Januari 2020",
+                  style: donationSubTextItalic,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: -40,
+          child: Container(
+            height: 85,
+            width: MediaQuery.of(context).size.width - 55,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12, spreadRadius: 5.5, blurRadius: 5.5)
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
